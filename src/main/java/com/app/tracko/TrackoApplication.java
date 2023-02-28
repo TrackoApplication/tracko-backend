d832319c7b0ce901eabdf3db099fdd638ff393b9
seefaBanu <banuags.20@uom.lk>
2023-02-28 16:24:22 +0530

    created springboot app

---
+++ src/main/java/com/app/tracko/TrackoApplication.java
@@ -0,0 +1,13 @@
+package com.app.tracko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/tracko/TrackoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.tracko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
