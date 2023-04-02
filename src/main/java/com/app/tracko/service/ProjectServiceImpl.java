783881532db1a6a1e2dd5ac76f2faef9b8ce8d52
Yasi <113851370+YasiruBasura@users.noreply.github.com>
2023-04-02 19:22:03 +0530

    ProjectAPI initialization complete

--- src/main/java/com/app/tracko/service/ProjectServiceImpl.java
+++ src/main/java/com/app/tracko/service/ProjectServiceImpl.java
@@ -1,6 +1,15 @@
 package com.app.tracko.service;
+import com.app.tracko.repository.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ProjectServiceImpl implements ProjectService {
 
+    private final ProjectRepository projectRepository;
 
+
+    public ProjectServiceImpl(ProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
 }
